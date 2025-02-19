4fc761152669ea568074505dfc656160e0cba432
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-02-18 21:27:27 -0500

    Added input text box to Calculator

--- obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f26343712843fc8f2daea105224f90dbd3f499dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea068203836b17ca696eb701fc131d9fabf5d292")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
