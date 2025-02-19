e6fefef508a0a27f1f060e29ce54d5f4bbeac5ae
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-02-19 09:54:57 -0500

    Button click makes input show in output

--- obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315e4884346aacc68c044d151d99997e66322a5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb656d9011037f505ab05f581627d12c5f64f52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
