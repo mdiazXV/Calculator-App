629401ce3cba7970a9487c89ed39b6097bf9cba6
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-02-19 11:10:14 -0500

    limited input to numbers rather than text

--- obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0cebd60ef359724932d5d1e2c1223e5a19a252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8eed56fc55f0ad799f7e60a02ed8c7f0087f37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
