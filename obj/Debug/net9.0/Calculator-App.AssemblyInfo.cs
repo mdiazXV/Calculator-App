ab8eed56fc55f0ad799f7e60a02ed8c7f0087f37
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-02-19 10:33:00 -0500

    concat two inputs on button click

--- obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e53972d6911e30e9a75dd3f30f5ee3c157d36424")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0cebd60ef359724932d5d1e2c1223e5a19a252")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
