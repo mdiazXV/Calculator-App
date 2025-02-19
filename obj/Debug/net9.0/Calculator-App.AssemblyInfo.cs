5efd1e41b7c074679ba0483e032947fdf531d0a3
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-02-19 10:05:33 -0500

    only first input displays when clicking enter

--- obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb656d9011037f505ab05f581627d12c5f64f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fefef508a0a27f1f060e29ce54d5f4bbeac5ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
