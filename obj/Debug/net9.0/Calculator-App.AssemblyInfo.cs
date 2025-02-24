a58e01aef74a92937a6172ad84d4bd3e12941a53
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-02-24 09:30:21 -0500

    added using system to calc csharp file

--- obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a23c70bfbc9abf5dd1da7454ad86b669dd371e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ade3d58d79faa4a6ff05af75953fdb96bad76c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
