c00ac26de61f8540da43b9b71c00e5e812ad0945
Mariah <48637409+mdiazXV@users.noreply.github.com>
2025-02-27 14:40:27 -0500

    I don't even know

--- obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd7530f7fa7fc082fa2ce4bbc5372766f39830d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b923b57264506c848c13ce4ba5914c216d659245")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
